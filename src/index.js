f33fbf2fb944d92ed2ba10d9e9a0540ba566e037
suguloga <lcsugit@gmail.com>
2018-10-27 16:48:56 -0700

    Begin coming up with desired API

--- src/index.js
+++ src/index.js
@@ -3,7 +3,32 @@ import PropTypes from "prop-types";
 
 import styles from "./styles.css";
 
-export const simpleMiddleWare = store => next => action => {
+/*
+  init state with { socket: socket(url) }
+*/
+export const socket = url => {
+  // Create the socket
+  const s = WebSocket(url);
+
+  // Send incoming socket events to the reducer
+  s.onmessage = event => {
+    const action = JSON.parse(event.data);
+    dispatch(action);
+  };
+
+  // Return this instance of the socket
+  return s;
+};
+
+/*
+  capture any socketed requests by the middleware and send to the socket
+*/
+export const socketMiddleware = store => next => action => {
   console.log("This is the action", action);
   next(action);
 };
+
+/*
+  Create a specific wrapper for a type to define a socketed type
+*/
+const SEND_SOCKET = type => `socket/${type}`;
